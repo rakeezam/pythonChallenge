cb5ac300fb3fb5586b222df61fd0a5ca453dbbbc
Adelani Oyawoye <Adelani72@gmail.com>
2020-12-17 21:51:21 +0000

    Use local storage to persist cart Data

--- Documents/and_Golden/and-golden/src/components/shop/Product.js
+++ Documents/and_Golden/and-golden/src/components/shop/Product.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useLocalStorage } from "react";
 import axios from "axios";
 import {
     BrowserRouter as Router,
@@ -7,8 +7,16 @@ import {
     useRouteMatch,
     useParams,
     Link
-  } from "react-router-dom";
-import {GetProduct} from './Shop';
+} from "react-router-dom";
+import { GetProduct } from './Shop';
+
+function AddToCartButton(props) {
+    if (props.stock === 0) {
+        return (<button type="button" disabled>Out of Stock</button>);
+    }
+    return (<button type="button" onClick={props.addToCart()}>Add To Cart</button>);
+
+}
 
 function Product() {
     let { id } = useParams();
@@ -16,28 +24,35 @@ function Product() {
     const [product, setResponseData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [errorLoading, setHasErrorLoading] = useState(false);
-  
-  
+
     useEffect(() => {
-        console.log(url);
-      axios({
-        "method": "GET",
-        "url": url,
-        "headers": {
-          "content-type": "application/json",
-        }
-      })
-        .then((product) => {
-          console.log(product);
-          setResponseData(product.data);
-          setIsLoading(false);
+        axios({
+            "method": "GET",
+            "url": url,
+            "headers": {
+                "content-type": "application/json",
+            }
         })
-        .catch((error) => {
-          console.log(error)
-          setHasErrorLoading(true);
-        })
+            .then((product) => {
+                setResponseData(product.data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.log(error)
+                setHasErrorLoading(true);
+            })
     }, [product.data])
-  
+    
+  function handleClick(e) {
+    e.preventDefault();
+    let cartItemsString = localStorage.getItem('shoppingCart');
+    let cartItemsArray = cartItemsString ? JSON.parse(cartItemsString) : [];
+    cartItemsArray.push(product);
+
+    localStorage.setItem('shoppingCart', JSON.stringify(cartItemsArray));
+    console.log(product._id);
+  }
+
     if (isLoading) {
         return <h1>Loading</h1>;
     }
@@ -49,7 +64,7 @@ function Product() {
             <h3>Stock: {product.stock}</h3>
             <h3>Stock: £{(product.price / 100).toFixed(2)}</h3>
             <img src={"http://localhost:5000/images/" + product._id + ".jpeg"} alt={product.name + "image"} />
-            {/* <GetProduct /> */}
+            <AddToCartButton stock={product.stock} addToCart={(e) => handleClick}></AddToCartButton>
 
         </div>
     );
